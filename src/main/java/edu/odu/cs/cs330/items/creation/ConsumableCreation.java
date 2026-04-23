ee14b9bcbdb10a35145c27dcab3dfcf3f9faf5d0
Kiowatt <nfree012@odu.edu>
2026-04-23 18:15:51 -0400

    Update ConsumableCreation.java

--- src/main/java/edu/odu/cs/cs330/items/creation/ConsumableCreation.java
+++ src/main/java/edu/odu/cs/cs330/items/creation/ConsumableCreation.java
@@ -29,7 +29,7 @@ public class ConsumableCreation implements ItemCreationStrategy
     public int requiredNumberOfValues()
     {
         // What is the correct return value?
-        return 3;
+        return 4;
     }
 
     @Override
@@ -37,8 +37,8 @@ public class ConsumableCreation implements ItemCreationStrategy
     {
         // Maybe call a Constructor that accepts multiple arguments...
         return new Consumable(
-            tokens[0],
             tokens[1],
+            tokens[2],
             Integer.parseInt(tokens[3]));
     }
 
